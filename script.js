2b55eba49bd00a5e3807ebc4f376c9c111be06b9
BrotatoBoi2point0 <134523755+BrotatoBoi2point0@users.noreply.github.com>
2024-10-26 16:38:26 -0700

    Typo and word changes.

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function change_frame(toFrame) {
         frame.src = './home.html';
     } else if(toFrame == 'games') {
         frame.src = './games.html';
-    } else if(toFrame == 'tutorials') {
-        frame.src = './tutorials.html';
+    } else if(toFrame == 'knowledge') {
+        frame.src = './knowledge.html';
     } else if(toFrame == 'other') {
         frame.src = './other.html';
     }
